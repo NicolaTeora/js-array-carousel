eb1026f739e7d927ec631a7e59f9f7d3986658b5
NicolaTeora <nikxodia@gmail.com>
2024-01-15 19:22:22 +0100

    feat : debug script

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,21 @@
-const gallerySlide = ['01.webp','02.webp','03.webp','04.webp','05.webp' ];
-
-
+const imgGallerySlide = document.getElementById('img-gallery-slide');
+const gallerySlide = ['01.webp', '02.webp', '03.webp', '04.webp', '05.webp'];
 
 let imgHTML = '';
+
 for (let i = 0; i < gallerySlide.length; i++) {
+    
     const imgGallery = gallerySlide[i];
-    imgHTML = `<img src="./img/${imgGallery}" class="slide" alt="image ${i}">`;
+    //console.log(imgGallery)
+    let visibleElement ='';
+    if (i == 0) {
+        visibleElement = 'visible'
+    } else {
+        visibleElement ='';
+    }
+    
+    imgHTML += `<img src="./img/${imgGallery}" class="slide ${visibleElement}" alt="image ">`;
 }
 
-console.log(imgHTML);
+imgGallerySlide.innerHTML = imgHTML;
+
