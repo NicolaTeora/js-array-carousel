da04aedd56b6ec5f2075021dfd9c3c08c649b0b6
NicolaTeora <nikxodia@gmail.com>
2024-06-24 12:25:33 +0200

    feat: addEventListner

--- JS/main.js
+++ JS/main.js
@@ -1,67 +1,64 @@
-const imgGallerySlide = document.getElementById('img-gallery-slide');
-const gallerySlide = ['01.webp', '02.webp', '03.webp', '04.webp', '05.webp'];
-const goUp = document.querySelector('.img_up');
-const goDown = document.querySelector('.img_down');
+const imgGallerySlide = document.getElementById("img-gallery-slide");
+const gallerySlide = ["01.webp", "02.webp", "03.webp", "04.webp", "05.webp"];
+const goUp = document.querySelector(".img_up");
+const goDown = document.querySelector(".img_down");
 
-let imgHTML = '';
+let imgHTML = "";
 
 for (let i = 0; i < gallerySlide.length; i++) {
-    
-    const imgGallery = gallerySlide[i];
-    let visibleElement ='';
-    if (i == 0) {
-        visibleElement = 'visible'
-    } else {
-        visibleElement ='';
-    }
-    
-    imgHTML += `<img src="./img/${imgGallery}" class="slide ${visibleElement}" alt="image ">`;
+  const imgGallery = gallerySlide[i];
+  let visibleElement = "";
+  if (i == 0) {
+    visibleElement = "visible";
+  } else {
+    visibleElement = "";
+  }
+
+  imgHTML += `<img src="./img/${imgGallery}" class="slide ${visibleElement}" alt="image ">`;
 }
 
 imgGallerySlide.innerHTML = imgHTML;
 
 slideMoment = 0;
-goDown.addEventListener('click', function(){
+goDown.addEventListener("click", function () {
+  //recupero
+  const imgsTotal = document.getElementsByClassName("slide");
+  //rimuovo classe visible
+  const imgOld = imgsTotal[slideMoment];
+  imgOld.classList.remove("visible");
+  //incremento o riazzero
+  if (slideMoment >= imgsTotal.length - 1) {
+    slideMoment = 0;
+  } else {
+    slideMoment++;
+  }
+  //assegno classe visible
+  const nextImg = imgsTotal[slideMoment];
+  nextImg.classList.add("visible");
+});
 
-    //recupero
-    const imgsTotal = document.getElementsByClassName('slide');
-    //rimuovo classe visible
-    const imgOld = imgsTotal[slideMoment];
-    imgOld.classList.remove('visible');
-    //incremento o riazzero
-    if (slideMoment >= imgsTotal.length -1) {
-        slideMoment = 0;
-    } else {
-        slideMoment++;
-    }
-    //assegno classe visible
-    const nextImg = imgsTotal[slideMoment];
-    nextImg.classList.add('visible');
-  
-})
-
-goUp.addEventListener('click', function() {
-    //recupero
-    const imgsTotal = document.getElementsByClassName('slide');
-    //rimuovo classe visible
-    const imgOld = imgsTotal[slideMoment];
-    imgOld.classList.remove('visible');
-    //decremento o riparto dal fondo
-    if (slideMoment <= 0) {
-        slideMoment = imgsTotal.length -1;
-    } else {
-        slideMoment--;
-    }
-    //assegno classe visible
-    const nextImg = imgsTotal[slideMoment];
-    nextImg.classList.add('visible');
-})
+goUp.addEventListener("click", function () {
+  //recupero
+  const imgsTotal = document.getElementsByClassName("slide");
+  //rimuovo classe visible
+  const imgOld = imgsTotal[slideMoment];
+  imgOld.classList.remove("visible");
+  //decremento o riparto dal fondo
+  if (slideMoment <= 0) {
+    slideMoment = imgsTotal.length - 1;
+  } else {
+    slideMoment--;
+  }
+  //assegno classe visible
+  const nextImg = imgsTotal[slideMoment];
+  nextImg.classList.add("visible");
+});
 
 //setInterval su bottone
-setInterval(function(){
-    goUp.click()
-},3000);
+const goSlideAuto = setInterval(function () {
+  goUp.click();
+}, 3000);
 
-imgGallerySlide.addEventListener('mouseover', function(){
-    console.log('mouse intercettato');
-})
+imgGallerySlide.addEventListener("mouseover", function () {
+  clearInterval(goSlideAuto);
+});
